066842abd871fe0cd720702f7a231c342940a895
evan <95433637+evankagarise@users.noreply.github.com>
2023-03-15 05:12:32 -0400

    inital

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='text-white'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+    </div>
+  )
+}
+
+export default Nav
