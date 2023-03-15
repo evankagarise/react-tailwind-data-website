137c695648111708c13a1c1d7f2cb3f804b4f851
evan <95433637+evankagarise@users.noreply.github.com>
2023-03-15 08:31:24 -0400

    third comp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Analytics from "./components/Analytics";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Nav />
       <Hero />
+      <Analytics />
     </div>
   );
 }
